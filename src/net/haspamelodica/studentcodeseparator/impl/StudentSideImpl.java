166a7a992d8f78abb741c256c3a3ed8f995efcf0
Haspamelodica <melodicahaspa@gmail.com>
2022-01-14 16:32:59 +0100

    Added thoughts about some problems and solution ideas

--- src/net/haspamelodica/studentcodeseparator/impl/StudentSideImpl.java
+++ src/net/haspamelodica/studentcodeseparator/impl/StudentSideImpl.java
@@ -23,6 +23,18 @@ import net.haspamelodica.studentcodeseparator.annotations.StudentSidePrototypeMe
 import net.haspamelodica.studentcodeseparator.communicator.StudentSideCommunicator;
 import net.haspamelodica.studentcodeseparator.exceptions.InconsistentHierarchyException;
 
+// TODO find better names for StudentSideObject/Prototype and configuration annotations.
+// Problem: Superclasses/interfaces.
+// .Idea: Specify using regular Java superinterfaces: Student-side object class extends other student-side object class
+// ..Problem: What if a student class is reqired to override a class / interface from the standard library?
+// ...Idea: Use a prototype for that class.
+// ...Sub-problem: What if the student object should be passed to a standard library function (for example Collections.sort)?
+// ....Idea: Don't do that tester-side, but student-side.
+// Problem: Regular Java objects passed to student-side objects would have to be serialized. This shouldn't happen automatically.
+// .Idea: Specify serializers to use with annotations and provide default serializers for usual classes (String, List, Set, Map...)
+// ..Problem: what about non-immutable datastructures?
+// .Idea: specify default prototypes. Problem: need to duplicate standard library interface.
+// ..Benefit: Handles non-immutable datastructures fine.
 public class StudentSideImpl<REF> implements StudentSide
 {
 	private final ClassLoader					classLoader;

88c4dbd559ead4d37a57038aa04c18f4b4e60ead
Haspamelodica <melodicahaspa@gmail.com>
2022-04-18 14:08:55 +0200

    Sorter example: Host and port are now read as commandline arguments

--- examples/sorter/exercise/src/main/java/sorter/SorterExerciseClient.java
+++ examples/sorter/exercise/src/main/java/sorter/SorterExerciseClient.java
@@ -11,14 +11,24 @@ import net.haspamelodica.studentcodeseparator.refs.Ref;
 
 public class SorterExerciseClient
 {
-	private static final boolean	LOGGING	= false;
-	private static final String		HOST	= "localhost";
-	private static final int		PORT	= 1337;
+	private static final boolean LOGGING = false;
 
 	public static void main(String[] args) throws IOException, InterruptedException
 	{
+		if(args.length != 2)
+			throw new IllegalArgumentException("Usage: java " + SorterExerciseClient.class.getName() + " host port");
 		// This code will eventually be moved to the framework.
-		try(Socket sock = new Socket(HOST, PORT))
+		String host = args[0];
+		int port;
+		try
+		{
+			port = Integer.parseInt(args[1]);
+		} catch(NumberFormatException e)
+		{
+			throw new IllegalArgumentException("Port not a number: " + args[1]);
+		}
+
+		try(Socket sock = new Socket(host, port))
 		{
 			DataCommunicatorClient<Ref<Integer, Object>> client = new DataCommunicatorClient<>(
 					sock.getInputStream(), sock.getOutputStream());
--- examples/sorter/studentrunner/src/main/java/net/haspamelodica/studentcodeseparator/examples/sorter/studentrunner/SorterExerciseServer.java
+++ examples/sorter/studentrunner/src/main/java/net/haspamelodica/studentcodeseparator/examples/sorter/studentrunner/SorterExerciseServer.java
@@ -14,12 +14,23 @@ import net.haspamelodica.studentcodeseparator.refs.intref.owner.IDReferrer;
 
 public class SorterExerciseServer
 {
-	private static final boolean	LOGGING	= false;
-	public static final int			PORT	= 1337;
+	private static final boolean LOGGING = false;
 
 	public static void main(String[] args) throws IOException
 	{
-		try(ServerSocket serverSocket = new ServerSocket(PORT); Socket sock = serverSocket.accept())
+		if(args.length != 1)
+			throw new IllegalArgumentException("Usage: java " + SorterExerciseServer.class.getName() + " port");
+		// This code will eventually be moved to the framework.
+		int port;
+		try
+		{
+			port = Integer.parseInt(args[0]);
+		} catch(NumberFormatException e)
+		{
+			throw new IllegalArgumentException("Port not a number: " + args[1]);
+		}
+
+		try(ServerSocket serverSocket = new ServerSocket(port); Socket sock = serverSocket.accept())
 		{
 			DataCommunicatorServer<?> server = new DataCommunicatorServer<>(sock.getInputStream(), sock.getOutputStream(),
 					maybeWrapLoggingS(new DirectSameJVMCommunicatorServerSide<>(new WeakDirectRefManager<

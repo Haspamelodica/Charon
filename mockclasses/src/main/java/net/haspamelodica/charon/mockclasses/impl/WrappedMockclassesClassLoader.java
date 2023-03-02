879830dc194612bd20f358a40fba5cca3c6ec14c
Haspamelodica <melodicahaspa@gmail.com>
2023-03-03 00:33:58 +0100

    Fixed a very ugly classloading problem in the Mockclasses frontend

--- mockclasses/src/main/java/net/haspamelodica/charon/mockclasses/impl/WrappedMockclassesClassLoader.java
+++ mockclasses/src/main/java/net/haspamelodica/charon/mockclasses/impl/WrappedMockclassesClassLoader.java
@@ -4,7 +4,10 @@ import static net.haspamelodica.charon.mockclasses.MockclassesUtils.pseudoAddAll
 
 import java.io.IOException;
 import java.util.List;
+import java.util.function.Supplier;
 
+import net.bytebuddy.description.method.MethodDescription;
+import net.bytebuddy.description.type.TypeDefinition;
 import net.haspamelodica.charon.WrappedCommunicator;
 import net.haspamelodica.charon.communicator.StudentSideCommunicatorClientSide;
 import net.haspamelodica.charon.marshaling.MarshalingCommunicator;
@@ -53,11 +56,6 @@ public class WrappedMockclassesClassLoader implements AutoCloseable
 			StudentSideCommunicatorClientSide<?> communicator, Class<?>... forceDelegationClasses)
 	{
 		MockclassesMarshalingTransformer transformer = new MockclassesMarshalingTransformer();
-		// TODO make Serdeses configurable
-		MarshalingCommunicator<?> marshalingCommunicator = new MarshalingCommunicator<>(communicator, transformer,
-				PrimitiveSerDes.PRIMITIVE_SERDESES).withAdditionalSerDeses(List.of(StringSerDes.class));
-		DynamicInvocationHandler<?, ?, ?, ?, ?> invocationHandler = new MockclassesInvocationHandler(marshalingCommunicator, transformer);
-
 		//TODO feels very hardcoded. Would be fixed if we didn't prevent delegating to the parent altogether.
 		Class<?>[] forceDelegationClassesWithClassesNeededByCharon = pseudoAddAll(forceDelegationClasses,
 				// Delegate classes referenced by / stored in dynamically-generated classes to parent; don't define them ourself.
@@ -69,10 +67,87 @@ public class WrappedMockclassesClassLoader implements AutoCloseable
 		//TODO instead of preventing delegating to parent, just prevent all user classes (called code) appearing in any classloader above the DynamicClassLoader,
 		// and load those through an even "lower" classloader.
 		ClassLoader constantClassSetClassloader = new ClassSetClassLoader(null, forceDelegationClassesWithClassesNeededByCharon);
-		ClassLoader dynamicClassloader = new DynamicClassLoader<>(constantClassSetClassloader, interfaceProvider, transformer, invocationHandler);
+		// InvocationHandler needs to be lazy because the initialization code of MarshalingCommunicator contains method references,
+		// which use bootstrap methods, which call some standard library function, which calls getSystemClassLoader, which is not allowed
+		// since we are creating the system class loader here.
+		ClassLoader dynamicClassloader = new DynamicClassLoader<>(constantClassSetClassloader, interfaceProvider, transformer,
+				new LazyDynamicInvocationHandler<>(() ->
+				{
+					// TODO make Serdeses configurable
+					MarshalingCommunicator<?> marshalingCommunicator = new MarshalingCommunicator<>(communicator, transformer,
+							PrimitiveSerDes.PRIMITIVE_SERDESES).withAdditionalSerDeses(List.of(StringSerDes.class));
+					return new MockclassesInvocationHandler(marshalingCommunicator, transformer);
+				}));
 		ClassLoader userClassLoader = new RedefiningClassLoader(dynamicClassloader, parent);
 
 		transformer.setClassloader(userClassLoader);
 		return userClassLoader;
+	}
+
+	private static class LazyDynamicInvocationHandler<CCTX, MCTX, SCTX, TCTX, ICTX> implements DynamicInvocationHandler<CCTX, MCTX, SCTX, TCTX, ICTX>
+	{
+		private final Supplier<DynamicInvocationHandler<CCTX, MCTX, SCTX, TCTX, ICTX>> handlerSupplier;
+
+		private DynamicInvocationHandler<CCTX, MCTX, SCTX, TCTX, ICTX> invocationHandler;
+
+		public LazyDynamicInvocationHandler(Supplier<DynamicInvocationHandler<CCTX, MCTX, SCTX, TCTX, ICTX>> handlerProvider)
+		{
+			this.handlerSupplier = handlerProvider;
+		}
+
+		@Override
+		public CCTX createClassContext(TypeDefinition type)
+		{
+			return handler().createClassContext(type);
+		}
+
+		@Override
+		public SCTX createStaticMethodContext(CCTX classContext, MethodDescription method)
+		{
+			return handler().createStaticMethodContext(classContext, method);
+		}
+
+		@Override
+		public TCTX createConstructorContext(CCTX classContext, MethodDescription constructor)
+		{
+			return handler().createConstructorContext(classContext, constructor);
+		}
+
+		@Override
+		public MCTX createInstanceMethodContext(CCTX classContext, MethodDescription method)
+		{
+			return handler().createInstanceMethodContext(classContext, method);
+		}
+
+		@Override
+		public Object invokeStaticMethod(CCTX classContext, SCTX methodContext, Object[] args)
+		{
+			return handler().invokeStaticMethod(classContext, methodContext, args);
+		}
+
+		@Override
+		public ICTX invokeConstructor(CCTX classContext, TCTX constructorContext, Object receiver, Object[] args)
+		{
+			return handler().invokeConstructor(classContext, constructorContext, receiver, args);
+		}
+
+		@Override
+		public Object invokeInstanceMethod(CCTX classContext, MCTX methodContext, Object receiver, ICTX receiverContext, Object[] args)
+		{
+			return handler().invokeInstanceMethod(classContext, methodContext, receiver, receiverContext, args);
+		}
+
+		private DynamicInvocationHandler<CCTX, MCTX, SCTX, TCTX, ICTX> handler()
+		{
+			if(invocationHandler != null)
+				return invocationHandler;
+
+			synchronized(this)
+			{
+				if(invocationHandler != null)
+					return invocationHandler;
+				return invocationHandler = handlerSupplier.get();
+			}
+		}
 	}
 }

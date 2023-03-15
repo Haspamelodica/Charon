dd35283dbd3b6c8b8ad1e6ef61639867a8224201
Haspamelodica <melodicahaspa@gmail.com>
2023-03-15 14:33:52 +0100

    Removed unused stream ID from DataCommunicatorConstants

--- common/src/main/java/net/haspamelodica/charon/communicator/impl/data/DataCommunicatorConstants.java
+++ common/src/main/java/net/haspamelodica/charon/communicator/impl/data/DataCommunicatorConstants.java
@@ -5,6 +5,5 @@ public enum DataCommunicatorConstants
 	;
 
 	public static final int	THREAD_INDEPENDENT_COMMAND_STERAM_ID	= 0;
-	public static final int	THREAD_INDEPENDENT_REQUEST_STERAM_ID	= 1;
-	public static final int	FIRST_FREE_STREAM_ID					= 2;
+	public static final int	FIRST_FREE_STREAM_ID					= 1;
 }

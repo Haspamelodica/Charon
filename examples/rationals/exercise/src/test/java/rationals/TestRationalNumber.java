57bc578ac2676d87f3260012f3801db813a5454d
Haspamelodica <melodicahaspa@gmail.com>
2023-10-24 18:19:35 +0200

    Found concise pattern for making Charon stuff available in JUnit tests

--- examples/rationals/exercise/src/test/java/rationals/TestRationalNumber.java
+++ examples/rationals/exercise/src/test/java/rationals/TestRationalNumber.java
@@ -10,32 +10,30 @@ import net.haspamelodica.charon.StudentSide;
 import net.haspamelodica.charon.junitextension.CharonExtension;
 
 @ExtendWith(CharonExtension.class)
-public class TestRationalNumber
+public record TestRationalNumber(RationalNumber.Prototype RationalNumber)
 {
 	@Test
-	public void testBasic(StudentSide studentSide)
+	public void testBasic()
 	{
-		RationalNumber.Prototype RationalNumberP = studentSide.createPrototype(RationalNumber.Prototype.class);
-
-		RationalNumber zero = RationalNumberP.ZERO();
+		RationalNumber zero = RationalNumber.ZERO();
 		assertEquals(zero.approximateAsDouble(), 0);
 
-		RationalNumber rational42 = RationalNumberP.new_(42);
+		RationalNumber rational42 = RationalNumber.new_(42);
 		assertEquals(rational42.num(), 42);
 		assertEquals(rational42.den(), 1);
 		assertEquals(rational42.approximateAsDouble(), 42);
 
-		RationalNumber rational50 = rational42.add(RationalNumberP.new_(8));
+		RationalNumber rational50 = rational42.add(RationalNumber.new_(8));
 		assertEquals(rational50.num(), 50);
 		assertEquals(rational50.den(), 1);
 		assertEquals(rational50.approximateAsDouble(), 50);
 
-		RationalNumber rational25_9 = rational50.div(RationalNumberP.new_(18));
+		RationalNumber rational25_9 = rational50.div(RationalNumber.new_(18));
 		assertEquals(rational25_9.num(), 25);
 		assertEquals(rational25_9.den(), 9);
 		assertEquals(rational25_9.approximateAsDouble(), 25 / 9d);
 
-		RationalNumber rational125_6 = rational25_9.mul(RationalNumberP.new_(15, 2));
+		RationalNumber rational125_6 = rational25_9.mul(RationalNumber.new_(15, 2));
 		assertEquals(rational125_6.num(), 125);
 		assertEquals(rational125_6.den(), 6);
 		assertEquals(rational125_6.approximateAsDouble(), 125 / 6d);
@@ -47,9 +45,7 @@ public class TestRationalNumber
 	@Test
 	public void testDiv0(StudentSide studentSide)
 	{
-		RationalNumber.Prototype RationalNumberP = studentSide.createPrototype(RationalNumber.Prototype.class);
-
 		assertStudentThrows(studentSide, DivisionByZeroException.Prototype.class,
-				() -> RationalNumberP.new_(42).div(RationalNumberP.ZERO()));
+				() -> RationalNumber.new_(42).div(RationalNumber.ZERO()));
 	}
 }
